36b1f188db9f278cef66beb493042c96dad8e76f
Rishub Tamirisa <rishubt2@illinois.edu>
2022-02-15 04:57:06 -0600

    updated dimensions

--- src/gan.py
+++ src/gan.py
@@ -5,11 +5,11 @@ from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dense, Flatten
 from tensorflow.keras.utils import to_categorical
 from tensorflow.keras.optimizers import SGD
 
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dense, Flatten, Conv2DTranspose, LeakyReLU, BatchNormalization, Activation, Reshape
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dense, Flatten, Conv2DTranspose, LeakyReLU, BatchNormalization, Activation, Reshape, MaxPool2D
 
 
 class CGAN:
-    def build_generator(width, height, depth, channels=1, inputDim=100, outputDim=512):
+    def build_generator(width, height, depth, channels=3, inputDim=100, outputDim=128):
         model = Sequential()
         inputShape = (width, height, depth)
         chanDim = 3
@@ -21,10 +21,11 @@ class CGAN:
             Activation("relu"),
             BatchNormalization(),
             Reshape(inputShape),
-            Conv2DTranspose(20, (5, 5), strides=(2, 2), padding="same"),
+            Conv2DTranspose(32, (5, 5), strides=(2, 2), padding="same"),
             Activation("relu"),
             BatchNormalization(axis=chanDim),
             Conv2DTranspose(channels, (5, 5), strides=(2, 2), padding="same"),
+            MaxPool2D(pool_size=(4,4)),
             Activation("tanh"),
         ])
         return model
@@ -32,12 +33,12 @@ class CGAN:
     def build_discriminator(width, height, depth, alpha=0.2):
         inputShape = (height, width, depth)
         model = Sequential([
-            Conv2D(10, (5, 5), padding="same", strides=(2, 2), input_shape=inputShape),
+            Conv2D(32, (5, 5), padding="same", strides=(2, 2), input_shape=inputShape),
             LeakyReLU(alpha=alpha),
-            Conv2D(20, (5, 5), padding="same", strides=(2, 2)),
+            Conv2D(64, (5, 5), padding="same", strides=(2, 2)),
             LeakyReLU(alpha=alpha),
             Flatten(),
-            Dense(512),
+            Dense(128),
             LeakyReLU(alpha=alpha),
             Dense(1),
             Activation("sigmoid"),
