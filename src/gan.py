acad7e27063de1e529f9852bc72346a534825edf
Rishub Tamirisa <rishubt2@illinois.edu>
2022-02-15 11:29:41 -0600

    revamped model

--- src/gan.py
+++ src/gan.py
@@ -1,15 +1,15 @@
 import numpy as np
 from pathlib import Path
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dense, Flatten
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dense, Flatten, Dropout
 from tensorflow.keras.utils import to_categorical
 from tensorflow.keras.optimizers import SGD
 
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dense, Flatten, Conv2DTranspose, LeakyReLU, BatchNormalization, Activation, Reshape, MaxPool2D
+from tensorflow.keras.layers import Conv2DTranspose, LeakyReLU, BatchNormalization, Activation, Reshape, MaxPool2D
 
 
 class CGAN:
-    def build_generator(width, height, depth, channels=3, inputDim=100, outputDim=128):
+    def generator(width, height, depth, channels=3, inputDim=100, outputDim=128):
         model = Sequential()
         inputShape = (width, height, depth)
         chanDim = 3
@@ -21,27 +21,40 @@ class CGAN:
             Activation("relu"),
             BatchNormalization(),
             Reshape(inputShape),
-            Conv2DTranspose(32, (5, 5), strides=(2, 2), padding="same"),
+            Conv2DTranspose(32, (5, 5), strides=(8, 2), padding="valid"),
             Activation("relu"),
             BatchNormalization(axis=chanDim),
-            Conv2DTranspose(channels, (5, 5), strides=(2, 2), padding="same"),
-            MaxPool2D(pool_size=(4,4)),
+            Conv2DTranspose(channels, (5, 5), strides=(4, 4), padding="same"),
             Activation("tanh"),
         ])
         return model
 
-    def build_discriminator(width, height, depth, alpha=0.2):
-        inputShape = (height, width, depth)
+    def discriminator(x, y, z, alpha=0.2):
+        inputShape = (x, y, z)
         model = Sequential([
-            Conv2D(32, (5, 5), padding="same", strides=(2, 2), input_shape=inputShape),
+            Conv2D(64, (3, 3), padding="same", strides=(2, 2), input_shape=inputShape),
             LeakyReLU(alpha=alpha),
-            Conv2D(64, (5, 5), padding="same", strides=(2, 2)),
+            Conv2D(128, (3, 3), padding="same", strides=(2, 2)),
+            LeakyReLU(alpha=alpha),
+            Conv2D(128, (3, 3), padding="same", strides=(2, 2)),
+            LeakyReLU(alpha=alpha),
+            Conv2D(256, (5, 5), padding="same", strides=(2, 2)),
             LeakyReLU(alpha=alpha),
             Flatten(),
-            Dense(128),
-            LeakyReLU(alpha=alpha),
-            Dense(1),
-            Activation("sigmoid"),
+            Dropout(0.4),
+            Dense(1, activation="sigmoid"),
         ])
         return model
 
+    def gen_real(dataset, n_samples):
+        seed = np.randint(0, dataset.shape[0], n_samples)
+        x = dataset[seed]
+        y = np.ones((n_samples, 1))
+        return x, y
+    
+    def gen_noise(x, y, z, n_samples):
+        seed = np.rand(x * y * z * n_samples)
+        seed = -1 + seed * 2
+        X = seed.reshape((n_samples, x, y, z))
+        y = np.zeros((n_samples, 1))
+        return X, y
--- src/mapper.py
+++ src/mapper.py
@@ -1,14 +1,15 @@
 from pathlib import Path
 from tensorflow.keras.utils import image_dataset_from_directory as idfd
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
-
 from pathlib import Path
+# from tensorflow.keras.utils import 
+
 import tensorflow as tf
 import os
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 path = Path(dir_path)
-dir = str(path.parent.absolute()) + "/all-images"
+dir = str(path.parent.absolute()) + "/all-images/train/class_1"
 save_dir = str(path.parent.absolute()) + "/images"
 
 print(dir)
@@ -18,14 +19,22 @@ print(dir)
 # print (Path.cwd)
 
 class RetinalImages:
-    batch_size = 32
-    img_height = 700
-    img_width = 605
-    train_gen = ImageDataGenerator()
-    retinal_data = train_gen.flow_from_directory(directory=dir,
-        target_size=(img_height, img_width),
-        batch_size=batch_size)
-    retinal_data.shape = [img_height, img_width, 3]
+
+    dataset = tf.data.Dataset.list_files(dir+"/*.ppm")
+    for item in dataset:
+        image_raw = tf.io.read_file(item)
+        image = tf.image.decode_image(image_raw)
+
+
+
+    # batch_size = 32
+    # img_height = 700
+    # img_width = 605
+    # train_gen = ImageDataGenerator()
+    # retinal_data = train_gen.flow_from_directory(directory=dir,
+    #     target_size=(img_height, img_width),
+    #     batch_size=batch_size)
+    # retinal_data.shape = [img_height, img_width, 3]
 
 # train_ds = idfd(
 #   dir,
